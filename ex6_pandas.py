83166ed94278b34eb538359306da149a9178e1cb
manish <manishqa2016@gmail.com>
2018-06-05 18:34:50 -0400

    3rd Commit

--- ex6_pandas.py
+++ ex6_pandas.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
 df = pd.read_csv('M:\Python\\numpy\Pandas\data\data\gapminder.tsv',sep='\t')
-print(df)
+#print(df[['year','pop']])
+
+df_year_pop = df[['year','pop','country']]
+#print(df_year_pop)
+
+df_year_pop_true = ((df_year_pop['pop'] > 10267083) &
+                   (df_year_pop['year'] > 2005) & 
+                   (df_year_pop['country'] == 'United Kingdom'))
+#print(df_year_pop_true)
+
+print(df[df_year_pop_true])
